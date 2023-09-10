6f8d170f031aa20da7ded3c74f3c07e63c15e760
tarunSachan <117214735+Tarun-Sachan@users.noreply.github.com>
2023-09-10 12:25:33 +0530

    Update CartContainer.js

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -31,7 +31,7 @@ const CartContainer = () => {
         <hr />
         <div className="cart-total">
           <h4>
-            total<span>${total.toFixed(2)}</span>
+            total<span>Rs. {total.toFixed(2)*100}</span>
           </h4>
         </div>
         <button
